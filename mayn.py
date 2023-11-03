f09c0f0e611d3aea0dda8307b4e231d76aad46b1
leghenda <147236825+leghenda@users.noreply.github.com>
2023-11-03 20:59:06 +0400

    Add files via upload

---
+++ mayn.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+from bot_logic import gen_pass
+
+# Переменная intents - хранит привилегии бота
+intents = discord.Intents.default()
+# Включаем привелегию на чтение сообщений
+intents.message_content = True
+# Создаем бота в переменной client и передаем все привелегии
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send( gen_pass(10))
+    
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+bot.run("MTE2NTE5MzUyMjY2ODg5NjM1Ng.GNkMu7.7lOphm5EDsOKF8D3bvsEoE8xVpdbLcobxTnrYI")
